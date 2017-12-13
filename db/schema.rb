d41ca21a1ec52693d1ac9bf570996c18d7b68e41
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2017-12-13 20:15:07 +0900

    rename user password column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206143902) do
+ActiveRecord::Schema.define(version: 20171213105902) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "encrypted_password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
